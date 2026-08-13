c774b1b302854f34594713809165b6e976f954d7
Hossein <hosseinabedinipoco@gmail.com>
2026-08-13 14:21:09 +0330

    dockerize project

--- concurrentbalanceservice/src/main/java/com/example/concurrentbalanceservice/ConcurrentBalanceServiceApplication.java
+++ concurrentbalanceservice/src/main/java/com/example/concurrentbalanceservice/ConcurrentBalanceServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConcurrentbalanceserviceApplication {
+public class ConcurrentBalanceServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConcurrentbalanceserviceApplication.class, args);
+		SpringApplication.run(ConcurrentBalanceServiceApplication.class, args);
 	}
 
 }
