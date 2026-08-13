ac040f958448d95cb381e2e9c5a6eb562e12ed7d
Hossein <hosseinabedinipoco@gmail.com>
2026-08-13 12:54:42 +0330

    init

---
+++ concurrentbalanceservice/src/main/java/com/example/concurrentbalanceservice/ConcurrentbalanceserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.concurrentbalanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentbalanceserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentbalanceserviceApplication.class, args);
+	}
+
+}
---
+++ concurrentbalanceservice/src/test/java/com/example/concurrentbalanceservice/ConcurrentbalanceserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concurrentbalanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentbalanceserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
